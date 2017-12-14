af54aa089d056437d4915defd47774478925b3d1
Justin K <justin-kim1@hotmail.com>
2017-12-14 02:59:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase) {
   return ('I can\'t hear you!');
 }
-{else if (string.toUpperCase) 
+{
+  else if (string.toUpperCase) 
   return ('YES INDEED!');
 }
-{else if ('I love you, Grandma.') 
-  return ('I love you,too.');
+{
+  else if ('I love you, Grandma.') 
+  return ('I love you,too.'); }
 }
