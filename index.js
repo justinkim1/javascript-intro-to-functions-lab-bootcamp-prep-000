0e3836a05809e8d4e3bc14942290d96b8bd590a0
Justin K <justin-kim1@hotmail.com>
2017-12-14 03:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
- console.log('HELLO')
+ console.log('string.toUpperCase')
 }
 function logWhisper(string) {
  console.log('hello')
